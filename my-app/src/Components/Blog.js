18ef92ac950e852518f4d848b21ca23ac4afc845
Prachi Rane <pr14983@pu-pr14983-mac.local>
2020-03-15 19:42:01 +0530

    Commited third version

--- my-app/src/Components/Blog.js
+++ my-app/src/Components/Blog.js
@@ -15,8 +15,6 @@ import Typography from '@material-ui/core/Typography';
 import Collapse from '@material-ui/core/Collapse';
 
 
-
-
 const useStyles = makeStyles(theme => ({
   root: {
     maxWidth: '60%',
@@ -76,36 +74,11 @@ around the globe and reflect SAP's culture of diversity and innovation. SAP Labs
 entities, developing and constantly improving key SAP solutions.`
 const otherInfo = `SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
 solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
-around the globe and reflect SAP's culture of diversity and innovation. SAP Labs are SAP's core R&D entities, developing and constantly improving key SAP 
-solutions. These research and development locations are strategically located in high-tech clusters 
 around the globe and reflect SAP's culture of diversity and innovation.`
 const upvotes = '50+'
 const views = '60'
-
-
-const Blog = (props) => {
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
-
-    return (
+/*
+return (
       <div class="wrapper">
         <card className={classes.root} border={1}>
           <CardHeader
@@ -138,6 +111,54 @@ const Blog = (props) => {
           </BootstrapTooltip>
         </CardActions>
         </card>
+      </div>
+    )
+    */
+
+const Blog = (props) => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
+    return (
+      <div class="wrapper">
+        { props.blog ? (
+            <card className={classes.root} border={1}>
+            <CardHeader
+              title={ <Typography className={classes.title}><b>{props.blog['title']}</b></Typography> }
+            />
+            <CardHeader
+                className={classes.cardSpacing} 
+                avatar = { <Avatar src={require('./Images/img.jpg')}/> }
+                title={ <Typography className={classes.username}>{props.blog['user']['firstName'] + " " + props.blog['user']['lastName']}</Typography> }
+                subheader={ <Typography className={classes.extras}>{props.blog['lastModifiedAt']}</Typography> }
+            />
+            <CardContent className={classes.cardSpacing} >
+              <Typography className={classes.username}> {props.blog['content']} 
+                <Link href="#" className={classes.link} onClick={handleExpandClick}>(more)</Link>
+              </Typography>     
+            </CardContent>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography className={classes.username} paragraph>
+                  {otherInfo}
+                </Typography>
+              </CardContent>
+            </Collapse>
+            <CardActions disableSpacing className={classes.cardSpacing} >
+              <BootstrapTooltip title="Upvotes" placement="top">
+              <Button size="small" className={classes.button} startIcon={<ArrowUpwardIcon />}>{props.blog['upVotes']}</Button> 
+            </BootstrapTooltip>
+              <BootstrapTooltip title="Views" placement="top">
+                  <Button size="small" className={classes.button} startIcon={<VisibilityIcon />}>{props.blog['views']}</Button> 
+            </BootstrapTooltip>
+          </CardActions>
+          </card>
+          ) : null }
+          
       </div>
     )
   
--- my-app/src/Components/BlogList.js
+++ my-app/src/Components/BlogList.js
@@ -4,20 +4,75 @@ import Blog from './Blog';
 import Navbar from './Navbar';
 import Grid from '@material-ui/core/Grid'
 
+const jsonResponse = [
+    {
+        "id": "7f1df38e-c120-499d-8806-f90ebc63b234",
+        "title": "How much do you value solitude in your life?",
+        "content": "Have you ever noticed how most of us automatically start browsing our cell phones whenever we feel alone at a party? There might not be anything relevant to browse, but we still keep looking at the phone. Why? Because most of us are afraid of being alone. When we are with someone, we feel occupied. When we are alone, we feel restless and start fidgeting around to find something to do, be it looking at the cell phone or watching television. I find solitude the most important and underrated aspect in life. When I am with others, I am simply passing time. But the time when I am alone is the time when I am truly making the most of my time. Solitude gives me the chance to think deep, plan and utilize my time to the most. I spend most of my free time sitting alone in cafes, when I can think and do something productive. I like taking evening walks completely alone, because that gives me a chance to just refresh my thoughts and regain energy. Being with others will always consume your energy, while being alone will give you a chance to rejuvenate that energy. When someone asks me how I find time to do so many things, I tell them to start spending less time with others and more time with yourself. You will realize that you suddenly have a lot of free time to think, plan and act. You will feel more energised and productive. There is nobody who can be a better companion for you than yourself.",
+        "views": 69407,
+        "upVotes": 4002,
+        "createdAt": "2020-03-13T21:54:52.544+0000",
+        "lastModifiedAt": "2020-03-13T21:56:25.923+0000",
+        "user": {
+            "id": "92ae5f19-60e7-496b-bc9f-d71de83d1ce6",
+            "firstName": "Pratik",
+            "lastName": "Gawali",
+            "emailId": "pratik.r.gawali@gmail.com",
+            "credentials": "Developer",
+            "imageId": null
+        }
+    },
+    {
+        "id": "7f1df38e-c120-499d-8806-f90ebc63b234",
+        "title": "How much do you value solitude in your life?",
+        "content": "Have you ever noticed how most of us automatically start browsing our cell phones whenever we feel alone at a party? There might not be anything relevant to browse, but we still keep looking at the phone. Why? Because most of us are afraid of being alone. When we are with someone, we feel occupied. When we are alone, we feel restless and start fidgeting around to find something to do, be it looking at the cell phone or watching television. I find solitude the most important and underrated aspect in life. When I am with others, I am simply passing time. But the time when I am alone is the time when I am truly making the most of my time. Solitude gives me the chance to think deep, plan and utilize my time to the most. I spend most of my free time sitting alone in cafes, when I can think and do something productive. I like taking evening walks completely alone, because that gives me a chance to just refresh my thoughts and regain energy. Being with others will always consume your energy, while being alone will give you a chance to rejuvenate that energy. When someone asks me how I find time to do so many things, I tell them to start spending less time with others and more time with yourself. You will realize that you suddenly have a lot of free time to think, plan and act. You will feel more energised and productive. There is nobody who can be a better companion for you than yourself.",
+        "views": 69407,
+        "upVotes": 4002,
+        "createdAt": "2020-03-13T21:54:52.544+0000",
+        "lastModifiedAt": "2020-03-13T21:56:25.923+0000",
+        "user": {
+            "id": "92ae5f19-60e7-496b-bc9f-d71de83d1ce6",
+            "firstName": "Pratik",
+            "lastName": "Gawali",
+            "emailId": "pratik.r.gawali@gmail.com",
+            "credentials": "Developer",
+            "imageId": null
+        }
+    },
+    {
+        "id": "7f1df38e-c120-499d-8806-f90ebc63b234",
+        "title": "How much do you value solitude in your life?",
+        "content": "Have you ever noticed how most of us automatically start browsing our cell phones whenever we feel alone at a party? There might not be anything relevant to browse, but we still keep looking at the phone. Why? Because most of us are afraid of being alone. When we are with someone, we feel occupied. When we are alone, we feel restless and start fidgeting around to find something to do, be it looking at the cell phone or watching television. I find solitude the most important and underrated aspect in life. When I am with others, I am simply passing time. But the time when I am alone is the time when I am truly making the most of my time. Solitude gives me the chance to think deep, plan and utilize my time to the most. I spend most of my free time sitting alone in cafes, when I can think and do something productive. I like taking evening walks completely alone, because that gives me a chance to just refresh my thoughts and regain energy. Being with others will always consume your energy, while being alone will give you a chance to rejuvenate that energy. When someone asks me how I find time to do so many things, I tell them to start spending less time with others and more time with yourself. You will realize that you suddenly have a lot of free time to think, plan and act. You will feel more energised and productive. There is nobody who can be a better companion for you than yourself.",
+        "views": 69407,
+        "upVotes": 4002,
+        "createdAt": "2020-03-13T21:54:52.544+0000",
+        "lastModifiedAt": "2020-03-13T21:56:25.923+0000",
+        "user": {
+            "id": "92ae5f19-60e7-496b-bc9f-d71de83d1ce6",
+            "firstName": "Pratik",
+            "lastName": "Gawali",
+            "emailId": "pratik.r.gawali@gmail.com",
+            "credentials": "Developer",
+            "imageId": null
+        }
+    }
+]
+
+
 class BlogList extends Component {
 
     state  = {
-        courses : ["blog1", "blog2","blog3","blog4","blog5","blog5","blog6","blog7","blog8","blog9","blog10"]
+        blogs : jsonResponse
     }
     
     render() {
         return (
             <div class="wrapper">
                 <Navbar/>
-                {this.state.courses ? ( 
+                {this.state.blogs ? ( 
                     <div style={{flexGrow: 1, backgroundColor: '#F1F1F1'}}>
                         <Grid container>
-                            {this.state.courses.map(currentBlog => (
+                            {this.state.blogs.map(currentBlog => (
                                 <Grid item >
                                      <Blog blog={currentBlog} />
                                 </Grid>
